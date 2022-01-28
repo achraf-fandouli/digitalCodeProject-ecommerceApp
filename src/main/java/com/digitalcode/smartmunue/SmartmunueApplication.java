56677b8a6dc82a772f635e8d7dbf9685f5afb13a
Achraf FANDOULI <achref.fendouli@gmail.com>
2022-01-28 23:52:02 +0100

    first commit-spring security with default username 'user' and password generated automatically with spring security on the console

---
+++ src/main/java/com/digitalcode/smartmunue/SmartmunueApplication.java
@@ -0,0 +1,13 @@
+package com.digitalcode.smartmunue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartmunueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartmunueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalcode/smartmunue/SmartmunueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalcode.smartmunue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartmunueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
