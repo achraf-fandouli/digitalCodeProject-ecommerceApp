f07a8e7b480f43370cf4266022ea06cd342cfd65
Achraf FANDOULI <achref.fendouli@gmail.com>
2022-02-02 11:36:26 +0100

    CRUD product-ok

--- src/main/java/com/digitalcode/smartmunue/repository/ProductRepository.java
+++ src/main/java/com/digitalcode/smartmunue/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.digitalcode.smartmunue.repository;
 import com.digitalcode.smartmunue.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllProductByCategory_Id(int id);
 }
--- src/main/java/com/digitalcode/smartmunue/service/ProductService.java
+++ src/main/java/com/digitalcode/smartmunue/service/ProductService.java
@@ -1,11 +1,13 @@
 package com.digitalcode.smartmunue.service;
 
+import com.digitalcode.smartmunue.model.Category;
 import com.digitalcode.smartmunue.model.Product;
 import com.digitalcode.smartmunue.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -14,5 +16,21 @@ public class ProductService {
 
     public List<Product> getAllProduct() {
         return productRepository.findAll();
+    }
+
+    public void addProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    public void removeProductById(long id) {
+        productRepository.deleteById(id);
+    }
+
+    public Optional<Product> getProductById(long id) {
+        return productRepository.findById(id);
+    }
+
+    public List<Product> getAllProductByCategoryId(int id){
+        return productRepository.findAllProductByCategory_Id(id);
     }
 }
