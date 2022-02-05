661922b32c1877d5498986fb4bfa2a479ddc5e82
Achraf FANDOULI <achref.fendouli@gmail.com>
2022-02-05 18:58:21 +0100

    Default data Insertion in databases for Admin User Role and Simple User role

--- src/main/java/com/digitalcode/smartmunue/controller/LoginController.java
+++ src/main/java/com/digitalcode/smartmunue/controller/LoginController.java
@@ -1,11 +1,20 @@
 package com.digitalcode.smartmunue.controller;
 
+import com.digitalcode.smartmunue.model.Role;
+import com.digitalcode.smartmunue.model.User;
 import com.digitalcode.smartmunue.repository.RoleRepository;
 import com.digitalcode.smartmunue.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class LoginController {
@@ -29,5 +38,16 @@ public class LoginController {
         return "register";
     }
 
+    @PostMapping("/register")
+    public String registerost(@ModelAttribute("user")User user, HttpServletRequest httpServletRequest) throws ServletException {
+        String password= user.getPassword();
+        user.setPassword(bCryptPasswordEncoder.encode(password));
+        List<Role> roles= new ArrayList<>();
+        roles.add(roleRepository.findById(2).get());
+        user.setRoles(roles);
+        userRepository.save(user);
+        httpServletRequest.login(user.getEmail(),password);
+        return "redirect:/";
+    }
 
 }
